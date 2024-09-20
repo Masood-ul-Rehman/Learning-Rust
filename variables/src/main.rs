902115e49b6840c68475068ec1cadc6578f42673
Masood ul Rehman <masooddulrehman@gmail.com>
2024-09-20 16:25:32 +0500

    learned about shadowing variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
-    let age-32;
+    let age = 32;
     println!("Age is {}", age);
     let mut mutable_age = 32;
     mutable_age = 33;
     println!("Mutable age is {}", mutable_age);
-    const THREE_HOURS_IN_SECONDS:u64=3*60*60;
+    const THREE_HOURS_IN_SECONDS: u64 = 3 * 60 * 60;
     println!("Three hours in seconds is {}", THREE_HOURS_IN_SECONDS);
 
+    //shadowing
+    let age = 32;
+    let age = 33;
+    println!("Age is {}", age);
+
+    let x: i32 = 10;
+    let x: i32 = x + 9;
+    println!("x is {}", x);
+    {
+        let x: i32 = x - 1;
+        println!("x is {}", x);
+    }
+    println!("x is {}", x);
 }
