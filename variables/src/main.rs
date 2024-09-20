dca4d1f8f1b293042f227b40907653fece0a68f4
Masood ul Rehman <masooddulrehman@gmail.com>
2024-09-20 15:53:50 +0500

    learned let and const, mutable and non muteables

---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let age-32;
+    println!("Age is {}", age);
+    let mut mutable_age = 32;
+    mutable_age = 33;
+    println!("Mutable age is {}", mutable_age);
+    const THREE_HOURS_IN_SECONDS:u64=3*60*60;
+    println!("Three hours in seconds is {}", THREE_HOURS_IN_SECONDS);
+
+}
