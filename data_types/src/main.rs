03f88a928cc70df1f3ce5f0b725a5b4675e692e7
Masood ul Rehman <masooddulrehman@gmail.com>
2024-09-20 18:00:02 +0500

    learned datatypes

---
+++ data_types/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    let floating = 2.0;
+    let floating2:f64 = 3.032567434;
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+    
+}
