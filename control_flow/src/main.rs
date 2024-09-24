c209ee8344e7a2548e4b8b9657644cf90d7b4aac
Masood ul Rehman <masooddulrehman@gmail.com>
2024-09-24 14:57:46 +0500

    learned control flow

---
+++ control_flow/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut num = 0;
+    // loop {
+    //     println!("Hello, world! {}", num);
+
+    //     if (num == 10) 
+    //         {break;}
+        
+    //     num = num + 1;
+    // }
+
+    let mut lift = 3;
+
+    while lift != 0 {
+        println!("{}!!", lift);
+        lift -= 1;
+    }
+    println!("LIft off!!!");
+}
